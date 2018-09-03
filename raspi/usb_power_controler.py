37de3a21d58f1e52533f7618430773e23d29221a
suzu6 <y.suzumura0604@gmail.com>
2018-09-03 15:17:35 +0900

    Changes to be committed: 	new file:   raspi/usb_power_controler.py

---
+++ raspi/usb_power_controler.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8
+
+from firebase import firebase
+import subprocess
+import time
+
+counter = 0
+turnon = True
+
+# Initialize the app with a service account, granting admin privileges
+firebase = firebase.FirebaseApplication(
+    'https://acerola-4cf84.firebaseio.com',
+    None
+)
+
+while counter < 10:
+	time.sleep(5)
+	turnon = firebase.get('/devices/light/power', None)
+	print turnon
+
+	if turnon :
+		subprocess.call(["sh", "./usb_turn_on.sh"])
+	else :
+		subprocess.call(["sh", "./usb_turn_off.sh"])
+	counter += 1
+
+
