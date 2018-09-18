24da70f2f6a4dbb757615fd918d4908df926c646
suzu6 <y.suzumura0604@gmail.com>
2018-09-18 11:10:30 +0900

    a

--- raspi/switch.py
+++ raspi/switch.py
@@ -29,29 +29,29 @@ target = firebase.get('/setting/'+device, None)
 # 押下時にTrue、押されていない時はFalse
 done = False
 
-try:
-    while True:
-        # スイッチを押した時、状態をトグルする
-        # interval
-        time.sleep(1)
+# try:
+while True:
+    # スイッチを押した時、状態をトグルする
+    # interval
+    time.sleep(1)
 
-        value = GPIO.input(motion_pin)
-        print target, value
+    value = GPIO.input(motion_pin)
+    print target, value
 
-        if value == GPIO.HIGH and not done:
-            # switch on and not toggle power yet
-            turnon = firebase.get('/devices/'+target+'/power', None)
-            # 反転
-            turnon = not turnon
-            GPIO.output(led_pin, turnon)
-            turnon = firebase.put('/devices/'+target+'/power', turnon)
-            done = True
-            print target + ' is turn ' + 'on' if turnon else 'off'
-        else:
-            # switch off or done toggle power
-            done = False
+    if value == GPIO.HIGH and not done:
+        # switch on and not toggle power yet
+        turnon = firebase.get('/devices/'+target+'/power', None)
+        # 反転
+        turnon = not turnon
+        GPIO.output(led_pin, turnon)
+        turnon = firebase.put('/devices/'+target+'/power', turnon)
+        done = True
+        print target + ' is turn ' + 'on' if turnon else 'off'
+    else:
+        # switch off or done toggle power
+        done = False
 
-except:
-    print 'has error'
+# except:
+#     print 'has error'
 
 # subprocess.call(["sh", "/home/pi/acerola/raspi/run_usb.sh"])
